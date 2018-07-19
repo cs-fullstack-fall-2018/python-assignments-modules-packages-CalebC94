fcb53081ba8351dbbf3e9d391af2d399d810cb2a
Caleb Canada <calebcanada13@gmail.com>
2018-07-19 15:59:28 -0500

    check in

---
+++ MyMainPackage/Main.Sub.Script.py
@@ -0,0 +1,5 @@
+from MyMainPackage import SomeMainScript
+from MyMainPackage.SubPackage import SomeSubScript
+
+SomeMainScript.my_main_funct()
+SomeSubScript.main()
---
+++ MyMainPackage/SubPackage/SomeSubScript.py
@@ -0,0 +1,2 @@
+def main():
+    print("This is the sub module not the main module!")
